f59a03e18873dcd2839c8fc4c26a36548ecb1243
pruthvi1902 <119110120+pruthvi1902@users.noreply.github.com>
2023-02-07 18:23:58 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -14,7 +14,6 @@ NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
 set +x
 
 echo 'The following complex command behaves similarly to the previous one but'
-echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 set +x
